54128c900c7dbabaa6cc08322d2c48e2507e9816
the_badcoder <xktareq07@gmail.com>
2017-03-13 01:58:32 +0600

    Flood Fill
    Flood Fill

---
+++ Graph/Flood Fill/FloodFill.cpp
@@ -0,0 +1,6 @@
+const int dr[] = { -1, 1, 0, 0 }; /// 4 side moves.
+const int dc[] = { 0, 0, -1, 1 };
+const int dx[] = { 1, 1, 0, -1, -1, -1, 0, 1 }; /// 8 side moves.
+const int dy[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
+const int kr[] = { 1, 1, -1, -1, 2, 2, -2, -2 }; /// Knight Moves.
+const int kc[] = { 2, -2, 2, -2, 1, -1, 1, -1 };
