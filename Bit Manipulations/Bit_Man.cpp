bc1676e718f187ce9e3f411c89f9b3c7d5385c62
the_badcoder <xktareq07@gmail.com>
2017-03-13 02:00:48 +0600

    Bit Manipulations

---
+++ Bit Manipulations/Bit_Man.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define isOn(S, j) (S & (1 << j))
+#define setBit(S, j) (S |= (1 << j))
+#define clearBit(S, j) (S &= ~(1 << j))
+#define toggleBit(S, j) (S ^= (1 << j))
+#define lowBit(S) (S & (-S))
+#define setAll(S, n) (S = (1 << n) - 1)
+#define modulo(S, N) ((S) & (N - 1))   // returns S % N, where N is a power of 2
+#define isPowerOfTwo(S) (!(S & (S - 1)))
+#define nearestPowerOfTwo(S) ((int)pow(2.0, (int)((log((double)S) / log(2.0)) + 0.5)))
+#define turnOffLastBit(S) ((S) & (S - 1))
+#define turnOnLastZero(S) ((S) | (S + 1))
+#define turnOffLastConsecutiveBits(S) ((S) & (S + 1))
+#define turnOnLastConsecutiveZeroes(S) ((S) | (S - 1))
+
+void printSet(int vS)                           // in binary representation
+{
+    printf("S = %2d = ", vS);
+    stack<int> st;
+    while (vS)
+        st.push(vS % 2), vS /= 2;
+    while (!st.empty())                         // to reverse the print order
+        printf("%d", st.top()), st.pop();
+    printf("\n");
+}
